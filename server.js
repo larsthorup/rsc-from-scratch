ba402effc9dac8c7241983fe9c2c75532e164593
Lars Thorup <lars@zealake.com>
2023-11-18 12:29:59 +0100

    routing

--- server.js
+++ server.js
@@ -1,17 +1,63 @@
 import { createServer } from "http";
-import { readFile } from "fs/promises";
+import { readFile, readdir } from "fs/promises";
 import escapeHtml from "escape-html";
+import sanitizeFilename from "sanitize-filename";
 
 createServer(async (req, res) => {
-  const author = "Lars Thorup";
-  const postContent = await readFile("./posts/hello-world.txt", "utf8");
-  const jsx = h(BlogPostPage, { author, postContent });
-  // console.log(JSON.stringify(jsx, null, 2));
-  const html = renderJSXToHTML(jsx);
-  sendHTML(res, html);
+  try {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const page = await matchRoute(url);
+    // const postSlug = "hello-world";
+    // const postContent = await readFile(`./posts/${postSlug}.txt`, "utf8");
+    const jsx = h(BlogLayout, {}, page);
+    // console.log(JSON.stringify(jsx, null, 2));
+    const html = renderJSXToHTML(jsx);
+    sendHTML(res, html);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = err.statusCode ?? 500;
+    res.end();
+  }
 }).listen(8080);
 
-function BlogPostPage({ author, postContent }) {
+async function matchRoute(url) {
+  if (url.pathname === "/") {
+    // We're on the index route which shows every blog post one by one.
+    // Read all the files in the posts folder, and load their contents.
+    const postFiles = await readdir("./posts");
+    const postSlugs = postFiles.map((file) =>
+      file.slice(0, file.lastIndexOf("."))
+    );
+    const postContents = await Promise.all(
+      postSlugs.map((postSlug) =>
+        readFile("./posts/" + postSlug + ".txt", "utf8")
+      )
+    );
+    return h(BlogIndexPage, { postSlugs, postContents });
+  } else {
+    // We're showing an individual blog post.
+    // Read the corresponding file from the posts folder.
+    const postSlug = sanitizeFilename(url.pathname.slice(1));
+    try {
+      const postContent = await readFile(
+        "./posts/" + postSlug + ".txt",
+        "utf8"
+      );
+      return h(BlogPostPage, { postSlug, postContent });
+    } catch (err) {
+      throwNotFound(err);
+    }
+  }
+}
+
+function throwNotFound(cause) {
+  const notFound = new Error("Not found.", { cause });
+  notFound.statusCode = 404;
+  throw notFound;
+}
+
+function BlogLayout({ children }) {
+  const author = "Lars Thorup";
   return h(
     "html",
     {},
@@ -20,9 +66,38 @@ function BlogPostPage({ author, postContent }) {
       "body",
       {},
       h("nav", {}, h("a", { href: "/" }, "Home"), h("hr")),
-      h("article", {}, postContent),
+      h("main", {}, ...children),
       h(Footer, { author })
     )
+  );
+}
+
+function BlogIndexPage({ postSlugs, postContents }) {
+  return h(
+    "section",
+    {},
+    h("h1", {}, "Welcome to my blog"),
+    h(
+      "div",
+      {},
+      postSlugs.map((postSlug, index) =>
+        h(
+          "section",
+          { key: postSlug },
+          h("h2", {}, h("a", { href: "/" + postSlug }, postSlug)),
+          h("article", {}, postContents[index])
+        )
+      )
+    )
+  );
+}
+
+function BlogPostPage({ postSlug, postContent }) {
+  return h(
+    "section",
+    {},
+    h("h2", {}, h("a", { href: `/${postSlug}` }, postSlug)),
+    h("article", {}, postContent)
   );
 }
 
