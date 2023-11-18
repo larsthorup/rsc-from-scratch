013c0ff4d07b091714d0c6f6fe51d323167b0314
Lars Thorup <lars@zealake.com>
2023-11-18 12:09:04 +0100

    components

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ import escapeHtml from "escape-html";
 createServer(async (req, res) => {
   const author = "Lars Thorup";
   const postContent = await readFile("./posts/hello-world.txt", "utf8");
-  const jsx = h(
+  const jsx = h(BlogPostPage, { author, postContent });
+  // console.log(JSON.stringify(jsx, null, 2));
+  const html = renderJSXToHTML(jsx);
+  sendHTML(res, html);
+}).listen(8080);
+
+function BlogPostPage({ author, postContent }) {
+  return h(
     "html",
     {},
     h("head", {}, h("title", {}, "My blog")),
@@ -14,18 +21,19 @@ createServer(async (req, res) => {
       {},
       h("nav", {}, h("a", { href: "/" }, "Home"), h("hr")),
       h("article", {}, postContent),
-      h(
-        "footer",
-        {},
-        h("hr"),
-        h("p", {}, h("i", {}, `(c) ${author}, ${new Date().getFullYear()}`))
-      )
+      h(Footer, { author })
     )
   );
-  // console.log(JSON.stringify(jsx, null, 2));
-  const html = renderJSXToHTML(jsx);
-  sendHTML(res, html);
-}).listen(8080);
+}
+
+function Footer({ author }) {
+  return h(
+    "footer",
+    {},
+    h("hr"),
+    h("p", {}, h("i", {}, `(c) ${author}, ${new Date().getFullYear()}`))
+  );
+}
 
 function h(type, props = {}, ...children) {
   return {
@@ -48,21 +56,31 @@ function renderJSXToHTML(jsx) {
   } else if (typeof jsx === "object") {
     // Check if this object is a React JSX element (e.g. <div />).
     if (jsx.$$typeof === Symbol.for("react.element")) {
-      // Turn it into an an HTML tag.
-      let html = "<" + jsx.type;
-      for (const propName in jsx.props) {
-        if (jsx.props.hasOwnProperty(propName) && propName !== "children") {
-          html += " ";
-          html += propName;
-          html += "=";
-          html += escapeHtml(jsx.props[propName]);
+      if (typeof jsx.type === "string") {
+        // Is this a tag like <div>?
+        // Turn it into an an HTML tag.
+        let html = "<" + jsx.type;
+        for (const propName in jsx.props) {
+          if (jsx.props.hasOwnProperty(propName) && propName !== "children") {
+            html += " ";
+            html += propName;
+            html += "=";
+            html += escapeHtml(jsx.props[propName]);
+          }
         }
-      }
-      html += ">";
-      html += renderJSXToHTML(jsx.props.children);
-      html += "</" + jsx.type + ">";
-      return html;
-    } else throw new Error("Cannot render an object.");
+        html += ">";
+        html += renderJSXToHTML(jsx.props.children);
+        html += "</" + jsx.type + ">";
+        return html;
+      } else if (typeof jsx.type === "function") {
+        // Is it a component like <BlogPostPage>?
+        // Call the component with its props, and turn its returned JSX into HTML.
+        const Component = jsx.type;
+        const props = jsx.props;
+        const returnedJsx = Component(props);
+        return renderJSXToHTML(returnedJsx);
+      } else throw new Error("Not implemented.");
+    }
   } else throw new Error("Not implemented.");
 }
 
